194abbcab72bb36506a6bc69610a356f5b97631b
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2020-03-07 20:21:47 -0300

    adicionando pods

---
+++ ConversorDeValores/ConversorDeValores/Controller/MoedaOrigemViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MoedaOrigemViewController.swift
+//  ConversorDeValores
+//
+//  Created by Gabriel Sanzone on 07/03/20.
+//  Copyright © 2020 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MoedaOrigemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ ConversorDeValores/ConversorDeValores/Controller/MoedaParaConverterTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MoedaParaConverterTableViewController.swift
+//  ConversorDeValores
+//
+//  Created by Gabriel Sanzone on 07/03/20.
+//  Copyright © 2020 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import Foundation
+
+
+class MoedaParaConverterTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
